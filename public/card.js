f36b8fa58e7fbd0f6cc220d9d1afb78f27444499
Juan <juanmanuelarias@gmail.com>
2017-10-15 20:28:03 +1300

    add header effect

--- public/card.js
+++ public/card.js
@@ -1,8 +1,24 @@
-$(window).load(function() {
-    $('.post-module').hover(function() {
-      $(this).find('.description').stop().animate({
-        height: "toggle",
-        opacity: "toggle"
-      }, 300);
+scrollHeader();
+cardToggle();
+
+function cardToggle() {
+    $(window).load(function() {
+        $('.post-module').hover(function() {
+            $(this).find('.description').stop().animate({
+                height: "toggle",
+                opacity: "toggle"
+            }, 300);
+        });
     });
-  });
+}
+
+function scrollHeader() {
+    $(document).on("scroll", function() {
+        if ($(document).scrollTop() > 100) {
+            $("header").addClass("header-shrink");
+        }
+        else {
+            $("header").removeClass("header-shrink");
+        }
+    });
+}
